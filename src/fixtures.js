a62d6bfa05885dc24a1a459fa53b7c03bc66720f
Alex Ivanov <ivanov.alex1@gmail.com>
2019-01-17 13:23:48 +0200

    first-commit

---
+++ src/fixtures.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        "id": " 56sdkhjaf743742",
+        "date":"2016-07-09T15:03:23.000Z",
+        "title":"LOREM IPSUM DOLL",
+        "text": "In the context of ORM, database auditing means tracking and logging events related to persistent entities, or simply entity versioning. Inspired by SQL triggers, the events are insert, update and delete operations on entities. The benefits of database auditing are analogous to those provided by source version control.\n" +
+        "\n" +
+        "We will demonstrate three approaches to introducing auditing into an application. First, we will implement it using standard JPA. Next, we will look at two JPA extensions that provide their own auditing functionality: one provided by Hibernate, another by Spring Data.",
+        "comments": [
+            {
+                "id": "whighreihe",
+                "user": "William Griffin",
+                "text": "Werry exciting"
+            },
+            {
+                "id": "htoiweov",
+                "user": "Gloria Payne",
+                "text": "Werry exciting"
+            }
+
+        ]
+    } ,
+    {
+        "id": " 5qrqo1rg",
+        "date":"2018-12-06T02:15:23.000Z",
+        "title":"LOREM IPSUM DOLL",
+        "text": "In the context of ORM, database auditing means tracking and logging events related to persistent entities, or simply entity versioning. Inspired by SQL triggers, the events are insert, update and delete operations on entities. The benefits of database auditing are analogous to those provided by source version control.\n" +
+        "\n" +
+        "We will demonstrate three approaches to introducing auditing into an application. First, we will implement it using standard JPA. Next, we will look at two JPA extensions that provide their own auditing functionality: one provided by Hibernate, another by Spring Data.",
+        "comments": [
+            {
+                "id": "whighreihe",
+                "user": "William Griffin",
+                "text": "Werry exciting"
+            },
+            {
+                "id": "htoiweov",
+                "user": "Gloria Payne",
+                "text": "Werry exciting"
+            }
+
+        ]
+    }
+]
